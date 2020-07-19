f0f6212f976985831d379f568e8b29021a9849d1
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2020-07-19 13:43:44 +0200

    Creating template files
    README.md editing, environment configuration

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # alerabat.com-OLX
 Discount codes.
+
+## Description
+Description...
+
+## Technologies
+* REACT
+* ES6
+* JSON
+* CSS, Sass
+* Webpack, Gulp
+
+## Futures
+
+## Demo
+App demo...
+
+## Install
+* clone repository
+
+## ToDo
+* web layout
+* CSS style
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const entryPath = "development";
+const entryFile = "src/js/App.js";
+module.exports = {
+    entry: ["whatwg-fetch", `./${entryPath}/${entryFile}`],
+    output: {
+        filename: "out.js",
+        path: path.resolve(__dirname, `${entryPath}/build`),
+    },
+    devServer: {
+        contentBase: path.join(__dirname, `${entryPath}`),
+        publicPath: "/build/",
+        compress: true,
+        port: 3000,
+        historyApiFallback: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader",
+            },
+        ],
+    },
+};

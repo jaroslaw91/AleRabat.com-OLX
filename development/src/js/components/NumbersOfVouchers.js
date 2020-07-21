ebacbdfdb1c7fb1fe02a56fcee2dfaa47f19c975
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2020-07-21 17:26:38 +0200

    Create NumbersOfVouchers component
    Added contents and styles

---
+++ development/src/js/components/NumbersOfVouchers.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NumbersOfVouchers = () => {
+    return (
+        <>
+            <h2 className="numbers-of-vouchers">15525 kodów rabatowych i promocji w jednym miejscu</h2>
+        </>
+    );
+}
+
+export default NumbersOfVouchers;

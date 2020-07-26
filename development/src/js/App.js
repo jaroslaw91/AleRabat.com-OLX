8e21a5b85209243504afe7ca6244a04e80b2c024
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2020-07-26 15:23:04 +0200

    Created Navigatiom component
    Added Navigation component, style and Router, added font and code fix

--- development/src/js/App.js
+++ development/src/js/App.js
@@ -1,14 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+} from "react-router-dom";
 import "./../scss/main.scss";
 import Headers from "./components/Header";
-import NumbersOfVouchers from "./components/NumbersOfVouchers";
+import Heading from "./components/Heading";
+import Search from "./components/Search";
+import Navigation from "./components/Navigation";
 
 const App = () => {
     return (
         <>
             <Headers />
-            <NumbersOfVouchers />
+            <Heading />
+            <Search />
+            <Navigation />
+            <Router>
+                <>
+                    <Switch>
+                        {/* <Route exact path="/" component={Main} /> */}
+                        {/* <Route path="/top" component={Top} /> */}
+                        {/* <Route path="/sklepy" component={Shops} /> */}
+                    </Switch>
+                </>
+            </Router>
         </>
     );
 }

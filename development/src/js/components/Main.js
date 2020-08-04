8ac73d4a7045bb23e4e7bbe924fd7725b014706c
Jarosław Kucharski <jaroslawkucharski91@gmail.com>
2020-08-04 10:11:21 +0200

    Built Main component
    Added popular shops section with slider (beginnings of build)

--- development/src/js/components/Main.js
+++ development/src/js/components/Main.js
@@ -1,8 +1,26 @@
 import React from "react";
 
-const Main = () => {
+const Main = ({ shops }) => {
     return (
-        <h2>Najpopularniejsze sklepy</h2>
+        <>
+            <h2>Najpopularniejsze sklepy</h2>
+            <div className="shops-list">
+                <div className="shops-list--cover">
+                    <div />
+                    <div />
+                    <div />
+                </div>
+                <div className="shops-list--container">
+                    <ul>
+                        {shops?.map(m => (
+                            <li key={m.id}>
+                                <img src={"./../../assets/img/shops/" + m.id + ".webp"} />
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 }
 
